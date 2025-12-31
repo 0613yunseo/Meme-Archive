ab909cb2018bfa98867abbe6f1692a970616dc02
ssuno <ssuno@example.com>
2025-12-31 18:14:03 +0900

    feat: API v1 구현

--- backend/src/main/java/com/meme/backend/repository/MemeRepository.java
+++ backend/src/main/java/com/meme/backend/repository/MemeRepository.java
@@ -7,7 +7,9 @@ import java.util.List;
 
 public interface MemeRepository extends JpaRepository<Meme, Long> {
 
+    // 좋아요 순 TOP 3
     List<Meme> findTop3ByOrderByLikesDesc();
 
+    // 최신순 TOP 6
     List<Meme> findTop6ByOrderByCreatedAtDesc();
 }
